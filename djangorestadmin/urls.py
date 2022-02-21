8ad1f7e88676a1fbd9df6de310a3336e2d807685
marc-hg <marcoshornogarcia@gmail.com>
2022-02-21 21:09:11 +0100

    chganges

--- djangorestadmin/urls.py
+++ djangorestadmin/urls.py
@@ -12,7 +12,7 @@ router.register(r'users', UserViewSet)
 router.register(r'users', UserCreateViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('api/v1/', include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
--- djangorestadmin/users/admin.py
+++ djangorestadmin/users/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import Person, User
 
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
     pass
+
+admin.site.register(Person)
